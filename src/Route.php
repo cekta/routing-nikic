cacef48cbe016663964ccfd847747ef54b3ab1e0
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-09-09 22:23:00 +0500

    init

---
+++ src/Route.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Cekta\Routing\Nikic;
+
+use Cekta\Routing\RouteInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class Route implements RouteInterface
+{
+    /**
+     * @var RequestHandlerInterface
+     */
+    private $handler;
+    /**
+     * @var array
+     */
+    private $attributes;
+    /**
+     * @var MiddlewareInterface[]
+     */
+    private $middlewares;
+
+    public function __construct(RequestHandlerInterface $handler, array $attributes, MiddlewareInterface ...$middlewares)
+    {
+        $this->handler = $handler;
+        $this->attributes = $attributes;
+        $this->middlewares = $middlewares;
+    }
+
+    public function getHandler(): RequestHandlerInterface
+    {
+        return $this->handler;
+    }
+
+    /**
+     * @return MiddlewareInterface[]
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+}
